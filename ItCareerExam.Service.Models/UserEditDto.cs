aa0fc2d521c57d9b300cab5cb145a4ed67a6c8a8
MartinKaraivanov <87902145+MartinKaraivanov@users.noreply.github.com>
2023-06-25 15:45:23 +0300

    added EmailAddress to Email in the UserEditDto

--- ItCareerExam.Service.Models/UserEditDto.cs
+++ ItCareerExam.Service.Models/UserEditDto.cs
@@ -1,8 +1,11 @@
-﻿namespace ItCareerExam.Service.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItCareerExam.Service.Models;
 
 public class UserEditDto
 {
     public required string Id { get; set; }
+    [EmailAddress]
     public required string Email { get; set; }
     public required string Password { get; set; }
     public required string UserName { get; set; }
